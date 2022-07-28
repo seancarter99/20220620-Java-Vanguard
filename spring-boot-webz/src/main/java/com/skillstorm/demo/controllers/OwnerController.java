531c763b973f66b52aaac56a40ce026324f16620
seancarter99 <scarter@skillstorm.com>
2022-07-28 11:37:34 -0400

    Added @Query annotation example and logging

--- spring-boot-webz/src/main/java/com/skillstorm/demo/controllers/OwnerController.java
+++ spring-boot-webz/src/main/java/com/skillstorm/demo/controllers/OwnerController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +24,23 @@ import com.skillstorm.demo.services.OwnerService;
 @RestController
 @RequestMapping("/owners")
 public class OwnerController {
+	
+	/*
+	 * Loggers allow us to flip a switch on certain log levels in certain environments
+	 * This way, I don't have to delete any debug prints, they'll just be disabled
+	 * 
+	 * Log Levels:
+	 * TRACE: A specific type of debug more pertaining to control flow
+	 * DEBUG: These are used for debug prints. They don't make it to production
+	 * INFO: Informational. Ex. Printing the IP address on a web request
+	 * WARN: Something went wrong, but was recovered from. Worth looking into
+	 * ERROR: Larger issues that could break the app. Ex. Third party API is down
+	 * FATAL: Application cannot continue as is. Ex. Database is gone
+	 * 
+	 * If I set the log level to info. Then only info-fatal logs are printed. Debug and trace do not happen
+	 */
+	
+	private static final Logger log = LoggerFactory.getLogger(OwnerController.class);
 	
 	@Autowired
 	private OwnerService service;
@@ -42,12 +61,16 @@ public class OwnerController {
 	// /owners/1/pets
 	@GetMapping("/{id}/pets")
 	public List<Pet> findPetsByOwnerId(@PathVariable int id) {
+		log.warn("Finding pets with owner id of " + id);
+//		System.out.println("Finding pets with owner id of " + id); // These prints are frowned upon in industry
 		return service.findPetsByOwnerId(id);
 	}
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Owner save(@Valid @RequestBody Owner owner) {
+//		System.out.println("Got here!"); // This never it to production
+		log.trace("Got here!");
 		return service.save(owner);
 	}
 }
